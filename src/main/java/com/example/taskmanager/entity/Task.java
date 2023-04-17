2f77efd6d6c65f734709fa914f06732940e5059c
acer <hatefzamany9@gmail.com>
2023-04-17 17:58:11 +0330

    use lombok constructor

--- src/main/java/com/example/taskmanager/entity/Task.java
+++ src/main/java/com/example/taskmanager/entity/Task.java
@@ -1,12 +1,16 @@
 package com.example.taskmanager.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,8 +24,4 @@ public class Task {
     private String title;
 
     private Boolean completed;
-
-    public Task() {
-
-    }
 }
--- src/main/java/com/example/taskmanager/entity/User.java
+++ src/main/java/com/example/taskmanager/entity/User.java
@@ -1,12 +1,16 @@
 package com.example.taskmanager.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
