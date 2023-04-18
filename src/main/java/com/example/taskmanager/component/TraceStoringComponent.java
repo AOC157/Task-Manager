784fc7cab4d59edfdf1815db9c5a27241bf40fd7
acer <hatefzamany9@gmail.com>
2023-04-18 20:34:36 +0330

    log requests and responses

--- src/main/java/com/example/taskmanager/component/TraceStoringComponent.java
+++ src/main/java/com/example/taskmanager/component/TraceStoringComponent.java
@@ -5,6 +5,8 @@ import com.example.taskmanager.repository.TraceRepository;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingResponseWrapper;
@@ -12,10 +14,12 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+
 @Component
 public class TraceStoringComponent implements Filter {
 
     private final TraceRepository traceRepository;
+    private static final Logger logger = LoggerFactory.getLogger(TraceStoringComponent.class);
 
     @Autowired
     public TraceStoringComponent(TraceRepository traceRepository) {
@@ -34,16 +38,28 @@ public class TraceStoringComponent implements Filter {
         byte[] responseArray = responseCacheWrapperObject.getContentAsByteArray();
         String responseStr = new String(responseArray, responseCacheWrapperObject.getCharacterEncoding());
 
+        logTrace(request, response, responseStr);
+
+        saveTrace(request, responseStr);
+
+        responseCacheWrapperObject.copyBodyToResponse();
+
+    }
+
+    private void logTrace(HttpServletRequest request, HttpServletResponse response, String responseStr) {
+        logger.info("Request: {} {} from {}", request.getMethod(), request.getRequestURI(), request.getRemoteAddr());
+        logger.info("Response of {} {}: {} with status {}", request.getMethod(), request.getRequestURI(), responseStr, response.getStatus());
+    }
+
+    private void saveTrace(HttpServletRequest request, String responseStr) {
         if (!request.getRequestURI().equals("/history")) {
             Trace trace = new Trace();
-            trace.setRequest(request.getRequestURI());
+            trace.setRequest(request.getMethod() + " " + request.getRequestURI());
             trace.setResponse(responseStr);
             trace.setDate(LocalDateTime.now());
 
             traceRepository.save(trace);
         }
-        responseCacheWrapperObject.copyBodyToResponse();
-
     }
 
 }
