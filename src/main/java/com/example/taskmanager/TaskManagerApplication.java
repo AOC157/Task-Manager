82304e4033a680e6ea842088da30b61c820b6084
acer <hatefzamany9@gmail.com>
2023-04-20 02:42:28 +0330

    add start running banner

--- src/main/java/com/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskManagerApplication.java
@@ -1,15 +1,32 @@
 package com.example.taskmanager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableCaching
 public class TaskManagerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TaskManagerApplication.class, args);
+    private static final Logger log = LoggerFactory.getLogger(TaskManagerApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(TaskManagerApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"));
     }
 
 }
