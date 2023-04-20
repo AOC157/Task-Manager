134f986ef85959da8533477234454811c51dadd9
acer <hatefzamany9@gmail.com>
2023-04-20 17:01:27 +0330

    add liquibase to create tables and load seed data

--- src/main/java/com/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskManagerApplication.java
@@ -7,15 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.core.env.Environment;
 
-import java.net.UnknownHostException;
-
 @SpringBootApplication
 @EnableCaching
 public class TaskManagerApplication {
 
     private static final Logger log = LoggerFactory.getLogger(TaskManagerApplication.class);
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) {
         SpringApplication app = new SpringApplication(TaskManagerApplication.class);
         Environment env = app.run(args).getEnvironment();
         String protocol = "http";
