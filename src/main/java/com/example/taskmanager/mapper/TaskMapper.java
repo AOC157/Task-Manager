bf67e4b41eaa2abafdd00cda1a5e79f210e26729
acer <hatefzamany9@gmail.com>
2023-04-20 19:06:59 +0330

    add comment to TaskMapper

--- src/main/java/com/example/taskmanager/mapper/TaskMapper.java
+++ src/main/java/com/example/taskmanager/mapper/TaskMapper.java
@@ -7,6 +7,11 @@ import org.mapstruct.Mapping;
 
 import java.util.List;
 
+/**
+ * <p>Resource: {@link com.example.taskmanager.mapper.TaskMapperImpl}</p>
+ * <p>Description: This class is used to map task entity to task dto. we will return dto instead of entity as APIs responses</p>
+ * <p>{@code TaskMapperImpl} is an auto-generated class which implements {@code TaskMapper} interface methods</p>
+ */
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
 
